6725836550f304ae3e6a7731e6cd3caeb91fa8d2
jaypawar2408 <147030830+jaypawar2408@users.noreply.github.com>
2023-10-12 11:35:06 +0530

    Add files via upload

---
+++ Mrudula6b.py
@@ -0,0 +1,11 @@
+class student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+s1=student("Samay",15)
+print(s1.name)
+print(s1.age)
+
+s2=("Rozi",18)
+print(s2.name)
+print(s2.age)
